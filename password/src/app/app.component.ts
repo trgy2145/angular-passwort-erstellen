2d441d83b5fae88ba124a6d2125af6ab863d7e6a
trgy2145 <trgy2145@gmail.com>
2022-05-05 23:22:29 +0200

    first commit

---
+++ password/src/app/app.component.ts
@@ -0,0 +1,78 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  password ='';
+  length:number = 0;
+  includeLetters:boolean = false;
+  includeNumbers:boolean = false;
+  includeSymbols:boolean = false;
+
+  modifyLength(value: string){
+      const parsedValue = parseInt(value);
+
+      if(!isNaN(parsedValue))
+      {
+        debugger;
+        this.length =parsedValue;
+      }
+  }
+
+
+  modifyLetters()
+  {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  modifyNumbers()
+  {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  modifySymbols()
+  {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  buttonClick() {
+   const numbers='1234567890';
+   const letters='abcdefghijklmnopqrstuvwyz';
+   const symbols='!@#$%^&*()';
+
+   let validChars='';
+
+   if(this.includeLetters)
+   {
+     validChars += letters;
+   }
+   if(this.includeNumbers)
+   {
+     validChars += numbers;
+   }
+   if(this.includeSymbols)
+   {
+     validChars += symbols;
+   }
+
+   let generatedPassword = '';
+   for(let i=0; i<this.length;i++)
+   {
+     const index=Math.floor(Math.random()*validChars.length);
+     generatedPassword +=validChars[index];
+   }
+
+   this.password=generatedPassword;
+
+
+
+
+
+
+  }
+
+}
